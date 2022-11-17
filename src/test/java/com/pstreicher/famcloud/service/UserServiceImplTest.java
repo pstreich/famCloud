70abcf739b29d0a279643155f4ff0fabea68e5a9
pstreicher <pstreichstrk@gmail.com>
2022-11-17 10:16:03 +0100

    refactored UnitTests

--- src/test/java/com/pstreicher/famcloud/service/UserServiceImplTest.java
+++ src/test/java/com/pstreicher/famcloud/service/UserServiceImplTest.java
@@ -1,39 +1,88 @@
 package com.pstreicher.famcloud.service;
 
 import com.pstreicher.famcloud.domain.UserInfo;
+import com.pstreicher.famcloud.repository.UserRepository;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.KeycloakSecurityContext;
 import org.keycloak.representations.IDToken;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.Authentication;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
+
+//@SpringBootTest
 class UserServiceImplTest {
 
-    @Autowired
-    private UserService userService;
+    UserServiceImpl userService;
 
-//    @Before
-//    void setup() {
-//        UserInfo userInfoJohn = new UserInfo();
-//    }
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    Authentication auth;
+
+    @Mock
+    KeycloakPrincipal<KeycloakSecurityContext> kPrincipal;
+
+    static IDToken idTokenFixture;
+    static UserInfo userInfoFixture;
+    static KeycloakPrincipal<KeycloakSecurityContext> principalFixture;
+
+    @BeforeAll
+    public static void beforAll() {
+        idTokenFixture = createIdTokenFixture();
+        principalFixture = createPrincipalFixture();
+        userInfoFixture = createUserInfoFixture();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        userService = new UserServiceImpl(userRepository);
+    }
+
+    @Test
+    void getUser() {
+        when(auth.getPrincipal()).thenReturn(principalFixture);
+        when(userRepository.save(any())).thenReturn(userInfoFixture);
+
+        assertEquals(userInfoFixture,  userService.getUser(auth));
+    }
 
     @Test
     void createNewUser() {
+        assertEquals(userInfoFixture, userService.createNewUser(idTokenFixture));
+    }
+
+    private static UserInfo createUserInfoFixture() {
+        return new UserInfo().builder()
+                .username("username")
+                .firstName("John")
+                .lastName("Thompson")
+                .email("jthompson@example.com")
+                .build();
+    }
+
+    private static KeycloakPrincipal<KeycloakSecurityContext> createPrincipalFixture() {
+        KeycloakSecurityContext keycloakSecurityContext = new KeycloakSecurityContext(null, null, null, idTokenFixture);
+        return new KeycloakPrincipal<>("John", keycloakSecurityContext);
+    }
+
+    private static IDToken createIdTokenFixture() {
         IDToken idToken = new IDToken();
         idToken.setPreferredUsername("username");
         idToken.setGivenName("John");
         idToken.setFamilyName("Thompson");
         idToken.setEmail("jthompson@example.com");
 
-        UserInfo userInfo = new UserInfo().builder()
-                    .username("username")
-                    .firstName("John")
-                    .lastName("Thompson")
-                    .email("jthompson@example.com")
-                    .build();
-
-        assertEquals(userInfo, userService.createNewUser(idToken));
+        return idToken;
     }
 }
