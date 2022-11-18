c5e1ca448e3498451e468f5d766b1054ffe0d1c4
pstreicher <pstreichstrk@gmail.com>
2022-11-18 08:27:01 +0100

    - enrich AuthUtilTest.java, testing bad authZ and anonymous authZ

--- src/test/java/com/pstreicher/famcloud/util/AuthUtilTest.java
+++ src/test/java/com/pstreicher/famcloud/util/AuthUtilTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.MockitoAnnotations;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,6 +22,9 @@ class AuthUtilTest {
     @Mock
     SecurityContext securityContext;
 
+    @Mock
+    AnonymousAuthenticationToken anonymousAuthenticationToken;
+
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
@@ -34,6 +38,40 @@ class AuthUtilTest {
             mocked.when(SecurityContextHolder::getContext).thenReturn(securityContext);
             when(securityContext.getAuthentication()).thenReturn(auth);
             when(auth.isAuthenticated()).thenReturn(true);
+
+            //when
+            boolean isAuthZ = AuthUtil.isAuthorized();
+
+            //then
+            assertEquals(isAuthZExpected, isAuthZ);
+        }
+    }
+
+    @Test
+    void isNotAuthorized() {
+        try (MockedStatic<SecurityContextHolder> mocked = mockStatic(SecurityContextHolder.class)) {
+            //given
+            boolean isAuthZExpected = false;
+            mocked.when(SecurityContextHolder::getContext).thenReturn(securityContext);
+            when(securityContext.getAuthentication()).thenReturn(auth);
+            when(auth.isAuthenticated()).thenReturn(false);
+
+            //when
+            boolean isAuthZ = AuthUtil.isAuthorized();
+
+            //then
+            assertEquals(isAuthZExpected, isAuthZ);
+        }
+    }
+
+    @Test
+    void isAnonymous() {
+        try (MockedStatic<SecurityContextHolder> mocked = mockStatic(SecurityContextHolder.class)) {
+            //given
+            boolean isAuthZExpected = false;
+            mocked.when(SecurityContextHolder::getContext).thenReturn(securityContext);
+            when(securityContext.getAuthentication()).thenReturn(anonymousAuthenticationToken);
+            when(anonymousAuthenticationToken.isAuthenticated()).thenReturn(true);
 
             //when
             boolean isAuthZ = AuthUtil.isAuthorized();
